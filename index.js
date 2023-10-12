308221d638891aa075390e8d01e8ea9b362b3a9a
Ammar-coder87 <ammar.kolko87@gmail.com>
2023-10-12 19:10:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const example = [
+const example1 = [
   ["A", "A", "A"],
   ["A", "A", "A"],
   ["A", "B", "A"]];
 
+  const example2 = [
+    ["O", "O", "O", "O"],
+    ["O", "O", "O", "O"],
+    ["O", "O", "O", "O"],
+    ["O", "O", "O", "O"],
+    ["P", "O", "O", "O"],
+    ["O", "O", "O", "O"]
+  ];
+
 const whereIsWaldo = (matrix) => {
-  
-}
+  let row = 1;
+  let colo = 1;
+  for( let i = 0; i < matrix.length; i++) {
+    for( let j = 0; j < matrix[i].length; j++) {
+      if(matrix[i][j] !== matrix[row][colo]) {
+        row = i;
+        colo = j;
+      }
+    }
+  }
+  return [row + 1, colo];
+};
+
+console.log(whereIsWaldo(example1));
+console.log(whereIsWaldo(example2));
+
